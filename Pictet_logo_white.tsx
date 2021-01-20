11a7431e8b6983f7086f46ec499ad058d37cbace
charafantah <github@charafantah.com>
2021-01-20 21:59:59 +0000

    Deploying to gh-pages from master @ 1eaa6c4af05144b1ad31689bb114a9f537706199 🚀

---
+++ Pictet_logo_white.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Pictet_logo_white" tilewidth="32" tileheight="32" tilecount="341" columns="31">
+ <image source="../../../Desktop/Delete me/Pictet_logo.jpg" width="1000" height="361"/>
+</tileset>
